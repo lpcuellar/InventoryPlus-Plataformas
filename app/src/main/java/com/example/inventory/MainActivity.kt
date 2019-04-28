461c3ffea00da9843b0478af26e63d411a446014
Luis Pero Cuéllar <lpcuellar14@gmail.com>
2019-04-27 18:19:49 -0600

    Losign full

--- app/src/main/java/com/example/inventory/MainActivity.kt
+++ app/src/main/java/com/example/inventory/MainActivity.kt
@@ -1,18 +1,26 @@
 package com.example.inventory
 
-import android.net.Uri
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.view.Menu
+import android.view.MenuInflater
+import com.example.inventory.R
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(),
-                        FragmentA.OnFragmentInteractionListener, FragmentB.OnFragmentInteractionListener {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
     }
 
-    override fun onFragmentInteraction(uri: Uri) {
-
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.example_menu, menu)
+        return true
     }
 }
